657ed8646793293257a457150bbd3cc4c87a2880
Dmitri <dmitri@momus.net>
2019-02-01 16:21:52 -0700

    chashRegister.makeChange() working

--- JavascriptAlgorithmsAndDataStructuresCertification/final-projects/cash-register.js
+++ JavascriptAlgorithmsAndDataStructuresCertification/final-projects/cash-register.js
@@ -1,31 +1,35 @@
 const currencyValue = {
-  "ONE HUNDRED": 100.0,
-  "TWENTY": 20.0,
-  "TEN": 10.0,
-  "FIVE": 5.0,
-  "ONE": 1.0,
-  "QUARTER": 0.25,
-  "DIME": 0.1,
-  "NICKEL" : 0.05,
-  "PENNY" : 0.01  
+    "ONE HUNDRED": 100.0,
+    "TWENTY": 20.0,
+    "TEN": 10.0,
+    "FIVE": 5.0,
+    "ONE": 1.0,
+    "QUARTER": 0.25,
+    "DIME": 0.1,
+    "NICKEL" : 0.05,
+    "PENNY" : 0.01  
 }
 
-const currencyOrder = Object.keys(currencyValue)//.reverse();
 
 const makeChange = (toChange, cid) => {
-  const drawer = cid.reverse();
-  let remainder = toChange;
-  return drawer.map((slot) => { //96.74
-    //slot format: ["DENOMINATION", quantaty]
-    const faceValue = currencyValue[slot[0]];
-    const coinsNeeded = Math.floor(remainder / faceValue);
-    const coinsInDrawer =  slot[1]  / faceValue; 
-    const coinQuant = Math.min(coinsNeeded,coinsInDrawer)  ;
-    remainder =  (remainder - (coinQuant * faceValue)).toFixed(2)  ; 
-    return coinQuant !== 0 ? [slot[0], coinQuant * faceValue] : "" ;
-            }).filter((x) => x !== "");
+    const drawer = cid.reverse();
+    let remainder = toChange;
+    let change =  drawer.map((slot) => { 
+        //slot format: ["DENOMINATION", quantaty]
+        const faceValue = currencyValue[slot[0]];
+        const coinsNeeded = Math.floor(remainder / faceValue);
+        const coinsInDrawer =  slot[1]  / faceValue; 
+        const coinQuant = Math.min(coinsNeeded,coinsInDrawer);
+        
+        remainder =  (remainder - (coinQuant * faceValue)).toFixed(2); 
+        return coinQuant !== 0 ? [slot[0], coinQuant * faceValue] : "" ;
+    }).filter((x) => x !== "");
+    
+    return remainder === "0.00" ? change : [];
 }
-  
+
+
+
 function checkCashRegister(price, cash, cid) {
   let change = makeChange(cash - price, cid);
   // Here is your change, ma'am.
